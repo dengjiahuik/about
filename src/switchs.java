7b26b7c8466eadf37636c76d985bac24294f386d
dengjiahuik <2648709234@qq.com>
2019-03-31 14:53:32 +0800

    更新

---
+++ src/switchs.java
@@ -0,0 +1,17 @@
+public class switchs {
+    public static void main(String[]args)
+    {int week=3;
+        switch(week)
+        {case 1:System.out.println("周一");break;
+            case 2:System.out.println("周二");break;
+            case 3:System.out.println("周三");break;
+            case 4:System.out.println("周四");break;
+            case 5:System.out.println("周五");break;
+            case 6:System.out.println("周六");break;
+            case 7:System.out.println("周末");break;
+            default:System.out.println("没有这一天");
+
+        }
+
+    }
+}
