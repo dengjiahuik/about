e6075d955252f2fb0c6d19979c9c698a47aa5c71
dengjiahuik <2648709234@qq.com>
2019-03-31 15:40:00 +0800

    更新

---
+++ src/elseif.java
@@ -0,0 +1,37 @@
+public class elseif {
+    public static void main(String[]args)
+    {int  week=4;
+    if (week==1)
+    {
+        System.out.println("周一");
+    }
+        else if (week==2)
+        {
+            System.out.println("周二");
+        }
+        else if (week==3)
+        {
+            System.out.println("周三");
+        }
+        else if (week==4)
+        {
+            System.out.println("周四");
+        }
+        else if (week==5)
+        {
+            System.out.println("周五");
+        }
+       else if (week==6)
+        {
+            System.out.println("周六");
+        }
+        else if (week==7)
+        {
+            System.out.println("周七");
+        }
+        else
+        {
+            System.out.println("没有这一天");
+        }
+    }
+}
