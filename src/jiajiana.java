1096ee77eea735243b059baadb16341a17641c06
dengjiahuik <2648709234@qq.com>
2019-04-02 19:01:51 +0800

    更新

---
+++ src/jiajiana.java
@@ -0,0 +1,21 @@
+public class jiajiana {
+    //package int练习
+    public static void main(String[] args) {
+        {
+            System.out.println(6 + 3);
+            System.out.println(6 - 3);
+            System.out.println(6 * 3);
+            System.out.println(6 / 3);
+            //开发中需求一直在变化
+            //需求变化 把操作改为8和4
+            int x = 6;
+            int y = 3;
+            System.out.println(x + y);
+            System.out.println(x - y);
+            System.out.println(x * y);
+            System.out.println(x / y);
+
+        }
+    }
+}
+
