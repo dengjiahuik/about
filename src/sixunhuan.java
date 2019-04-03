3252b4daa994d29be6aba619be0eab767fc1e078
dengjiahuik <2648709234@qq.com>
2019-04-03 18:18:22 +0800

    更新

---
+++ src/sixunhuan.java
@@ -0,0 +1,17 @@
+public class sixunhuan {
+    public static void main(String[] args) {//while死循环
+        /*while (true) {
+            System.out.println("你好");
+        }
+*/
+      /*  do
+            {
+            System.out.println("胡萝北");
+        } while (true);*/
+        for (; ;)
+        {
+            System.out.println("萝北做错了什么");
+        }
+    }
+}
+
