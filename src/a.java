4dba2bed31d8657d4a0427ac9e7270ce723dbc76
dengjiahuik <2648709234@qq.com>
2019-04-08 15:47:01 +0800

    更新

---
+++ src/a.java
@@ -0,0 +1,42 @@
+public class a {
+    public static void main(String[] args)
+        /*
+        输出矩形图案
+        *****
+        *****
+        *****
+         */
+        /*1
+        {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+    }*/
+        /*第二种{
+        for (int c = 1; c <= 5; c++) {
+                //自动换行:ln
+                System.out.print("*");
+        }
+        System.out.println();//换行
+        for (int c = 1; c <= 5; c++) {
+            //自动换行:ln
+            System.out.print("*");
+        }
+        System.out.println();//换行
+
+        for (int c = 1; c <= 5; c++) {
+            //自动换行:ln
+            System.out.print("*");
+        }
+        System.out.println();//换行
+    }*/ {
+        for (int b=1;b<=3;b++)
+        {
+        for (int c = 1; c <= 5; c++) {
+            //自动换行:ln
+            System.out.print("*");
+        }
+            System.out.println();//换行
+    }}
+}
+
