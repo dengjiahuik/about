6da266512cdd77ead9fed6d611930acbf2b7acf6
dengjiahuik <2648709234@qq.com>
2019-04-08 16:30:00 +0800

    更新

---
+++ src/b.java
@@ -0,0 +1,46 @@
+/**
+ * 输出三角形
+ * <p>
+ * *
+ * **
+ * ***
+ * ****
+ */
+
+public class b {
+    public static void main(String[] args)
+   /* {
+        for(int i=1;i<=1;i++);
+    {
+        System.out.print("*");
+    }
+        System.out.println();//换行
+        for(int i=1;i<=2;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();//换行
+        for(int i=1;i<=3;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();//换行
+        for(int i=1;i<=4;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();//换行
+        for(int i=1;i<=5;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();//换行
+}*/ {
+        for (int l = 1; l <= 5; l++) {
+            for (int i = 1; i <= l; i++) {
+                System.out.print("*");
+            }
+            System.out.println();//换行
+        }
+    }
+}
