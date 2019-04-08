88222d96f748f02be35fd1d8e2b28eea64e361b7
dengjiahuik <2648709234@qq.com>
2019-04-08 18:38:40 +0800

    更新

---
+++ src/jiujiu.java
@@ -0,0 +1,11 @@
+//输出九九乘法表
+public class jiujiu {
+    public static void main(String[] args) {
+        for (int i=1;i<=9;i++)
+        { for (int l=1;l<=i;l++)
+
+        System.out.println(i+"*"+l+"="+(i*l)+"\t");
+        System.out.println();//换行
+
+    }}
+}
